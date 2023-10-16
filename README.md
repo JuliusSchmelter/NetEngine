afef710542444ebf7667868d3a29b39b779ae8fe
Julius Schmelter <juliusschmelter@gmail.com>
2023-10-16 12:29:11 +0200

    cleanup

--- README.md
+++
@@ -1,4 +0,0 @@
-# NetEngine
-### A Simple Machine Learning Library
-
-Project for a university class.
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "NetEngine/DevTools.h"
-#include "NetEngine/Net.h"
-
-#include "MNIST_test.h"
-
-int main(void) {
-    MNIST_test();
-}
